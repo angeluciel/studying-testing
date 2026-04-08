0407cf1a14ad264152a0e4e1cbfcff0807616318
Joao-AZ <jpmizidoro@gmail.com>
2026-04-08 19:05:29 +0000

    frontend changes

--- src/frontend/app/login/page.tsx
+++ src/frontend/app/login/page.tsx
@@ -1,15 +1,12 @@
 import CustomHeader from "@/components/header";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-2 flex-1 items-center justify-start bg-background text-foreground font-sans">
       <CustomHeader />
-      <main className="h-[calc(100dvh-64px)] flex justify-center items-center">
-        <span>
-          <span>Welcome, wanna </span> {' '}
-          <Link href="/login" className="underline">login?</Link>
-        </span>
+      <main className="flex flex-col gap-2 justify-center items-center h-[calc(100dvh-64px)]">
+        <h1>Login</h1>
+        <div className="rounded-lg flex flex-col items-center justify-center"></div>
       </main>
     </div>
   );
---
+++ src/frontend/components/header.tsx
@@ -0,0 +1,5 @@
+export default function CustomHeader() {
+  return (
+    <header className="flex h-12 w-full justify-center md:justify-start items-center font-semibold px-8 border-b border-zinc-800">branduv + lucielmo</header>
+  )
+}

2b2c69a1612ec05a15173633958593a4fbe48c90
Joao-AZ <jpmizidoro@gmail.com>
2026-04-17 18:40:19 +0000

    route POST /users tests finished

--- src/backend/vitest.config.ts
+++ src/backend/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     silent: 'passed-only',
+    reporters: 'verbose',
 
     globals: true,
     environment: 'node',

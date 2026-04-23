e86b1264684ad9ab8b458a16511707b856b59fd4
Angeluciel <jpmizidoro@gmail.com>
2026-04-22 23:23:38 -0300

    fiz nada

--- src/backend/src/modules/auth/auth.routes.test.ts
+++ src/backend/src/modules/auth/auth.routes.test.ts
@@ -1,5 +1,5 @@
-import { describe, it  } from "vitest";
+import { describe, it } from 'vitest';
 
 describe('auth routes', () => {
-    it('placeholder', () => {});
-})
+  it.todo('placeholder', () => {});
+});

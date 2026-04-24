bd4555872b8c82ee1a133d403f9b319d2016febb
Joao-AZ <jpmizidoro@gmail.com>
2026-04-24 15:00:13 +0000

    added drizzle orm

--- src/backend/src/db/pool.ts
+++ src/backend/src/db/pool.ts
@@ -1,7 +1,10 @@
-import { Pool } from "pg";
-import { env } from "../config/env"
+import { Pool } from 'pg';
+import { env } from '../config/env';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 export const pool = new Pool({
-    connectionString: env.DATABASE_URL,
-    ssl: env.NODE_ENV == "production" ? { rejectUnauthorized: false } : false
+  connectionString: env.DATABASE_URL,
+  ssl: env.NODE_ENV == 'production' ? { rejectUnauthorized: false } : false,
 });
+
+const db = drizzle({ client: pool });
---
+++ src/backend/src/db/schema.ts
@@ -0,0 +1,31 @@
+import { pgTable, text, pgEnum, uniqueIndex, timestamp, boolean } from 'drizzle-orm/pg-core';
+import generateNanoId from '@/utils/idGen';
+import { timestamps } from '@/utils/columns.helper';
+import { uuid } from 'drizzle-orm/pg-core';
+
+const rolesEnum = pgEnum('role', ['user', 'admin']);
+
+export const usersTable = pgTable('users', {
+  id: text()
+    .primaryKey()
+    .$defaultFn(() => generateNanoId()),
+  email: text().notNull().unique(),
+  name: text().notNull(),
+  surname: text().notNull(),
+  password_hash: text().notNull(),
+  email_confirmed: boolean().default(false).notNull(),
+  is_active: boolean().default(true).notNull(),
+  role: rolesEnum().default('user').notNull(),
+  ...timestamps,
+});
+
+export const passwordChangeTokensTable = pgTable('password_change_tokens', {
+  id: uuid().primaryKey().defaultRandom().notNull(),
+  user_id: text()
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  token_hash: text().notNull(),
+  expires_at: timestamp().notNull(),
+  used_at: timestamp(),
+  created_at: timestamp().defaultNow().notNull(),
+});
